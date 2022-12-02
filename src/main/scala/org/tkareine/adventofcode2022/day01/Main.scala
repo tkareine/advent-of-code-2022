2a72e27b4d53a9ed1ebb1c42ac4a97829c1cc1d3
Tuomas Kareinen <tkareine@gmail.com>
2022-12-02 22:52:51 +0200

    Rename day 1 input

--- src/main/scala/org/tkareine/adventofcode2022/day01/Main.scala
+++ src/main/scala/org/tkareine/adventofcode2022/day01/Main.scala
@@ -3,7 +3,7 @@ package org.tkareine.adventofcode2022.day01
 import scala.collection.SortedSet
 import scala.io.StdIn.readLine
 
-// sbt 'runMain org.tkareine.adventofcode2022.day01.main' < input/calories-by-elves.txt
+// sbt 'runMain org.tkareine.adventofcode2022.day01.main' < input/day01.txt
 @main def main(): Unit = {
   var maxSums = scala.collection.mutable.SortedSet()(Ordering.Long.reverse)
   var currentSum = 0L
